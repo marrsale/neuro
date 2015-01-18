426e87d1d45b1ff3c1a835cb4bce694039125ac8
marrsale <marrsale@live.com>
2015-01-17 22:45:05 -0700

    WIP

---
+++ neuron.rb
@@ -0,0 +1,88 @@
+class Neuron
+  attr_accessor :weight
+  attr_reader :activation, :predecessors, :successors, :is_input, :bias_weight, :net_input
+
+  # Neuron#new
+  # Required arguments:
+  # - input_node (Boolean): true if the neuron is in the input layer
+  # - predecessors (Array): an array of neurons that precede the current one (will be empty for input layer neurons)
+  # - successors (Array): an array of neurons that succeed the current one (will be empty for output layer neurons)
+  # - activation_f (Proc): a lambda with the activation function (i.e. a logistic function for classification);
+  #                        if none provided, there is a default
+  #                        TODO: linear activation
+  # - deriv_f (Proc): a lambda with the derivative of the activation function, used for gradient descent backpropagation;
+  #                   if none provided, there is a default
+  #                   TODO: incorporate the derivator method for generic derivations when none provided
+  def initialize(opts={})
+    # pass block for non-default activation function (takes two arguments, a weight and a network input)
+    # initialize weight and references to predecessors/successors
+    @bias_weight  = 0
+    @input_node   = opts[:input_node] || false
+    @predecessors = opts[:predecessors] # array of neurons
+    @successors   = opts[:successors] # ''
+
+    # initialize the edges
+    # ... see Neuron#edge method for details
+    @edges = {}
+    if !(@input_node) # input nodes have no predecessors
+      @predecessors.each do |pred|
+        @edges[pred] = initialize_edge
+      end
+      # initialize the activation function and its derivative
+      # by default the activation function will be logistic
+
+      @activation_function = opts[:activation_f]  || -> x { 1/(1+Math.exp(-1*(x))) }
+      @activation_deriv    = opts[:deriv_f]       || -> y { y*(1-y) }
+      # TODO: if activation_f, but no deriv_f provided, call derivator(opts[:activation_f]) to create a lambda that is the approximate derivative
+    end
+  end
+
+
+  def net_input=(x)
+    @net_input = @activation = x
+  end
+
+  def output
+    if input?
+      # input nodes don't do any work
+      return @net_input
+    else
+      # apply the logistic function onto the activation and return it
+      @activation_function.call(@activation)
+    end
+  end
+
+  # Return the edge between self and a connected neuron
+  # note that a neuron only retains edges to its predecessor for simplicity
+  # however, if one has two connected neurons the edge can be accessed from either direction:
+  #   neuron1.edge(neuron2) #
+  #   neuron2.edge(neuron1) # both calls return the same result
+  def edge(neuron)
+    # the edge either belongs to self or the other neuron, go get it
+    @edges[neuron] || neuron.edge(self)
+  end
+
+  def input?
+    @input_node
+  end
+
+  private
+  # returns a randomized edge weight for when the neuron is created
+  # takes a range object, or defaults to a value between 0 and 1
+  def initialize_edge(range=(0..1.00))
+    rand range
+  end
+
+  # TODO: debug? test?
+  # returns a lambda that is the derivative function of a provided lambda argument
+  # the derivative is required for gradient descent; if the
+  def derivator(func)
+    if func.is_a? Proc
+      -> x {
+        ((func.(x+1e-13) - (func.(x-1e-13)) / (2e-13)
+      }
+    else
+      raise "Neuron#derivator must be given a Proc as an argument; #{func.class.name} was provided"
+    end
+  end
+end
--- train.rb
+++ train.rb
@@ -1,63 +1,77 @@
+require 'neuron'
 # Multilayer Perceptron
 class MLP
-  class Neuron
-    attr_accessor :weight
-    attr_reader :activation,:predecessors,:successors,:is_input
-
-
-    def initialize(opts={})
-      # pass block for non-default activation function (takes two arguments, a weight and a network input)
-      # initialize weight and references to predecessors/successors
-    end
-
-    def net_input=(x)
-      @activation = x
-    end
-
-    def output
-      if self.input?
-        return @activation
-      else
-        # apply the logistic function onto the activation and return it
-        @activation_function.call(@activation)
-      end
-    end
-
-    # return the edge between self and a connected neuron
-    def edge(neuron)
-      # the edge either belongs to self or the other neuron, go get it
-      if (edge = @edges[neuron] || neuron.edge(self))
-        edge
-      else
-        raise "Can't return edge.  #{self} isn't connected to #{neuron}"
-      end
-    end
-  end
-
   # NOTES
   # TODO: LEARNING RATE PARAM?
   #       - fix this at one for now, for pure gradient descent
   # TODO: ACTIVATION FUNCTIONS?
-  #       - (1/Math.exp((1.0 + Math.exp(-H * value))))
+  #       - default if one isn't provided
+  #       - (1/Math.exp((1.0 + Math.exp(-H * value))))  # sigmoid/logistic: classification
+  #       - (some other function here)                  # linear:
   # TODO: DERIVATIVES OF ACTIVATION FUNCTIONS?
   #       - y*(1-y)
+  # TODO: GENERIC DERIVATIVE?
+  #       - if a derivative isn't provided, generate a new lambda to calculate derivative using the activation function lambda
+
+  # returns a layer of neurons, i.e. an array of Neuron objects with appropriate propagation functions and initialized weights
+  def self.generate_layer!(opts={})
+    layer = opts[:array]
+    case opts[:type]
+    if opts[:type] == :input
+      opts[:size].times do
+        layer << Neuron.new(input: true,
+                            successors: opts[:successors])
+      end
+    else # generating a hidden or output layer
+      opts[:size].times do
+        layer << Neuron.new(predecessors: opts[:predecessors],
+                            successors: opts[:successors])
+      end
+    end
+  end
 
   # the network's initializer
   # MLP.new(key: value, key: value)
   def initialize(opts={})
+    # initialize layers
+    @input_size      = opts[:input_size]
+    @hidden_size     = opts[:hidden_size] || @input_size
+    @output_size     = opts[:output_size] || @input_size
+    @layers          = { input: [], hidden: [[]], output: [] }
+    self.generate_layer!(array: input, type: :input, successors: @layers[:hidden], size: @input_size)
+    self.generate_layer!(array: hidden, predecessors: @layers[:input], successors: @layers[:output], size: @hidden_size)
+    self.generate_layer!(array: output, predecessors: @layers[:hidden], size: @output_size)
+
+    # initialize math
     @learning_rate_param             = 0.2
     @propagation_function            = lambda { |x| 1/(1+Math.exp(-1*(x))) }
     @derivative_propagation_function = lambda { |y| y*(1-y) }
   end
 
+  def input
+    @layers[:input]
+  end
+
+  def hidden(n=0)
+    @layers[:hidden][n]
+  end
+
+  def output
+    @layers[:output]
+  end
+
   ##THE ALGORITHM (for a MLP with one hidden layer)
-  # training_iteration  = 1
-  # do # ... until the error term (or momentum) is satisfactorily minimized ceased
-  # mlp.train_pattern(input: [1,1], output: [0])
+  # example for training an MLP to learn the XOR function
+  # 4000.times do
+  #   @mlp.train_pattern(input: [1,1], output: [0])
+  #   @mlp.train_pattern(input: [0,1], output: [1])
+  #   @mlp.train_pattern(input: [1,0], output: [1])
+  #   @mlp.train_pattern(input: [0,0], output: [0])
+  # end
   def train_pattern(training_set)
     # FEED FORWARD
     # 1. Apply input vectors to input neurons
-    @training_inputs = vectorize(training_set[:input])
+    @training_inputs = training_set[:input]
     input.each.with_index do |neuron, j|
       neuron.net_input = @training_inputs[j]
     end
@@ -87,7 +101,7 @@ class MLP
     #    Don't apply the changes yet; calculate and set them aside until we calculate errors for whole network
     output_errors = output.map.with_index do |neuron, j|
       # TODO
-      result = (training[training_iteration][:output][j] - neuron.output)*(DERIVATIVE_OF_OUTPUT_ACTIVATION_FUNCTION_ON_NETINPUT)
+      result = (training_set[:output][j] - neuron.output)*(DERIVATIVE_OF_OUTPUT_ACTIVATION_FUNCTION_ON_NETINPUT)
     end
 
     # 7. Calculate the error terms for the hidden layer neurons
