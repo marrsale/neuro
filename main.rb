0ab38cbaeb8832ec3bd6e4fc239f6cb1a0620e5e
marrsale <marrsale@live.com>
2015-01-18 10:50:38 -0700

    Added autoencoder example to main rb

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require './mlp.rb'
-# Generate MLP with one hidden layer
+# Generate MLP with one hidden layer and train it to learn XOR
 @mlp = MLP.new(input_size: 2, hidden_size: 2, output_size: 1)
 100000.times do |n|
   @mlp.train_pattern!(input: [0,0], output: [0])
@@ -9,7 +9,19 @@ require './mlp.rb'
   puts "\nFor iteration \##{n}, error term is #{@mlp.last_error_term}.\n" if n != 0 && (n % 2000 == 0)
 end
 
-# train it to learn XOR
 [[1,1],[1,0],[0,0],[0,1]].each do |input_set|
   puts "For input #{input_set}\t#{@mlp.evaluate!(input_set)}"
+end
+
+# train an autoencoder
+@mlp2 = MLP.new(input_size: 3, hidden_size: 2, output_size: 3)
+100000.times do |n|
+  pattern = [[1,0].sample,[1,0].sample,[1,0].sample] # array of random bits
+  @mlp2.train_pattern!(input: pattern, output: pattern)
+  puts "\nFor iteration \##{n}, error term is #{@mlp2.last_error_term}.\n" if n != 0 && (n % 2000 == 0)
+end
+
+3.times do
+  pattern = [[1,0].sample,[1,0].sample,[1,0].sample]
+  puts "Pattern: #{pattern}\t#{@mlp2.evaluate!(pattern)}"
 end
