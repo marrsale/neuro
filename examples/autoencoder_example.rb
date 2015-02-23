0d51fae8b48b32ac973604ec5965f518c42dc969
marrsale <marrsale@live.com>
2015-02-22 19:22:49 -0700

    Fix autoencoder sample program

--- examples/autoencoder_example.rb
+++ examples/autoencoder_example.rb
@@ -1,13 +1,24 @@
 require '../neuro'
-# train an autoencoder
-autoencoder = MLP.new(input: 3, hidden: [2], output: 3)
-100000.times do |n|
-  pattern = [[1,0].sample,[1,0].sample,[1,0].sample] # array of random bits
-  autoencoder.train_pattern!(input: pattern, output: pattern)
-  puts "\nFor iteration \##{n}, error term is #{autoencoder.last_error_term}.\n" if n != 0 && (n % 2000 == 0)
+# Generate some random inputs for it to learn
+input_sets = []
+10.times do
+  input_sets << [[0,1].sample, [0,1].sample, [0,1].sample]
 end
 
-3.times do
-  pattern = [[1,0].sample,[1,0].sample,[1,0].sample]
-  puts "Pattern: #{pattern}\t#{autoencoder.evaluate!(pattern)}"
+# Instantiate the autoencoder
+autoencoder = ANN.new(input: 3, hidden: [2], output: 3)
+
+# Train it to recognize all of our inputs
+10000.times do |n|
+  input_sets.each do |set|
+    autoencoder.train_pattern!(input: set, output: set)
+  end
+
+  print "\rFor iteration \##{n}, error term is #{autoencoder.last_error_term}." if n != 0 && (n % 2000 == 0)
+end
+
+puts ''
+# Print out the result of the autoencoder for each
+input_sets.each do |set|
+  puts "For input: #{set},\tOutput: #{autoencoder.evaluate!(set).map { |obj| obj.round }}"
 end
--- examples/mlp_example.rb
+++ examples/mlp_example.rb
@@ -1,6 +1,8 @@
 require '../neuro'
-# Generate MLP with one hidden layer and train it to learn XOR
+# Generate MLP with one hidden layer
 mlp = ANN.new(input: 2, hidden: [2], output: 1)
+
+# Train the MLP to learn all the inputs (only 4)
 100000.times do |n|
   mlp.train_pattern!(input: [0,0], output: [0])
   mlp.train_pattern!(input: [0,1], output: [1])
@@ -9,6 +11,7 @@ mlp = ANN.new(input: 2, hidden: [2], output: 1)
   print "\rFor iteration \##{n}, error term is #{mlp.last_error_term}." if n != 0 && (n % 2000 == 0)
 end
 
+# Evaluate and output for each input
 print "\n"
 [[1,1],[1,0],[0,0],[0,1]].each do |input_set|
   puts "For input #{input_set}\t#{mlp.evaluate!(input_set)}"
